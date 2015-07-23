91fca96fe1fa3b54c29d944b49a5c87551263e18
Demo User <demouser@MacBook-Air.local>
2015-07-23 17:49:34 -0400

    .

--- index_script.js
+++ index_script.js
@@ -1,14 +1,19 @@
 $(function(){
 
-  $("#nameheader").hover(function(){
+$("#nameheader").hover(function(){
     $(this).fadeToggle(1000);
   });
 
-$("#mypic").click(function(){
-  $("<div><h3>Temporary Header</h3>Temporary Section</div><br />").appendTo("body");
+$("#moustache").click(function(){
+  $("<div><h3 class = 'temp'>Temporary Header</h3>Temporary Section</div><br />").appendTo("body");
   });
 
-
-
+$("#moustache").hover(function(){
+  $("#moustache").css("width","105px");
+  $("#moustache").css("background-color","white");
+},function(){
+  $("#moustache").css("width","100px");
+  $("#moustache").css("background-color","");
+  });
 
 });
