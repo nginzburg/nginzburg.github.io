c5d54d3f10af891106e3be84a18880ceccf1ff41
Demo User <demouser@MacBook-Air.local>
2015-07-24 09:37:46 -0400

    .

--- index_script.js
+++ index_script.js
@@ -5,7 +5,7 @@ $("#nameheader").hover(function(){
   });
 
 $("#moustache").click(function(){
-  $("<div><h3 class = 'temp'>Temporary Header</h3>Temporary Section</div><br />").appendTo("body");
+  $("<div>Temporary Header</h3>Temporary Section</div><br />").appendTo("body");
   });
 
 $("#moustache").hover(function(){
@@ -15,5 +15,54 @@ $("#moustache").hover(function(){
   $("#moustache").css("width","100px");
   $("#moustache").css("background-color","");
   });
+
+  $("#bio").click(function(){
+    $("#bioPic").slideDown(2000);
+  });
+
+  $("#bio").dblclick(function(){
+    $("#bioPic").slideUp(1000);
+  });
+
+  $("#bio").hover(function(){
+    $("#bio").text("Show Bio Android With Link to G+");
+  },function(){
+    $("#bio").text("Bio");
+  });
+
+//
+
+$("#interests").click(function(){
+  $("#intPic").slideDown(2000);
+});
+
+$("#interests").dblclick(function(){
+  $("#intPic").slideUp(1000);
+});
+
+$("#interests").hover(function(){
+  $("#interests").text("Chris Pratt");
+},function(){
+  $("#interests").text("Interests");
+});
+
+//
+//
+
+$("#cssipic").click(function(){
+  $("#imgsPic").slideDown(2000);
+});
+
+$("#cssipic").dblclick(function(){
+  $("#imgsPic").slideUp(1000);
+});
+
+$("#cssipic").hover(function(){
+  $("#cssipic").text("YAAASSSS");
+},function(){
+  $("#cssipic").text("Google CSSI");
+});
+
+//
 
 });
